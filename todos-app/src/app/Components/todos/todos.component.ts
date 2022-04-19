ee1c406a22539b769f46d51050a7ffa4e52853e5
qasem <2171246jo@s.aou.edu.jo>
2022-04-19 23:28:20 +0300

    ang

--- todos-app/src/app/Components/todos/todos.component.ts
+++ todos-app/src/app/Components/todos/todos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-todos',
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  Todos = [{id: 1, name: "Qasem", completed: false},
+    {id: 2, name: "Ali", completed: false},
+    {id: 3, name: "Ahmed", completed: false},
+    {id: 4, name: "Hassan", completed: false},
+    {id: 5, name: "KHALED", completed: false},
+    {id: 6, name: 'Mohammad', completed: false},
+    {id: 7, name: "Sami", completed: false},
+    {id: 8, name: "Sara", completed: false},
+    {id: 9, name: "Samer", completed: false},
+  ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
